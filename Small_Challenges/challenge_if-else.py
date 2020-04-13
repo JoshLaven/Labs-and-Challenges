2c30ae0a42d6eed6623a63e5e9b3f7cc1ca84c31
Sarah Gill <gills@uchicago.edu>
2020-04-13 08:32:45 -0500

    Update challenge_if-else.py
    made it lend itself to a nested if so a little more complicated

--- Small_Challenges/challenge_if-else.py
+++ Small_Challenges/challenge_if-else.py
@@ -2,23 +2,29 @@ initial_list = ['1', 2.4, 1, 'Hello World!', 2, 5, 400, 5.6890,
                  3.1415, 'It is fine', 4, 20, 49, 51.3]
 
 #output: a dictionary of lists
-#each value should be a list containing only one data type
+#each value should be a list containing only one data type,
+#if the data type is integer, sort odds and evens into different lists
 
-#Solution:
+#A Solution:
 
 string_list = []
-int_list = []
+even_int_list = []
+odd_int_list = []
 float_list = []
 
 for x in initial_list:
     if isinstance(x, str):
         string_list.append(x)
     elif isinstance(x, int):
-        int_list.append(x)
+        if x % 2 == 0:
+            even_int_list.append(x)
+        else:
+            odd_int_list.append(x)
     elif isinstance(x, float):
         float_list.append(x)
 
-my_dict = {'strings': string_list, 'ints': int_list, 'floats': float_list}
+my_dict = {'strings': string_list, 'evens': even_int_list, 
+            'odds': odd_int_list, 'floats': float_list}
 
 
 #w/o a for loop:
